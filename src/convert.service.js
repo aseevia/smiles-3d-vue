8a12a4717e1460158bdabef02c22da9a89c5cb92
mac <mac@macs-MacBook-Pro.local>
2018-06-05 20:49:44 +0300

    Minor fix on the retrofitted speck's function

--- src/convert.service.js
+++ src/convert.service.js
@@ -57,16 +57,16 @@ function validateSmiles(smile) {
 }
 
 function xyz2speck(data) {
-    var lines = data.split("\n");
-    var natoms = parseInt(lines[0]);
-    var nframes = Math.floor(lines.length / (natoms + 2));
-    var trajectory = [];
-    for (var i = 0; i < nframes; i++) {
-        var atoms = [];
-        for (var j = 0; j < natoms; j++) {
-            var line = lines[i * (natoms + 2) + j + 2].split(/\s+/);
-            var atom = {};
-            var k = 0;
+    let lines = data.split("\n");
+    let natoms = parseInt(lines[0]);
+    let nframes = Math.floor(lines.length / (natoms + 2));
+    let trajectory = [];
+    for (let i = 0; i < nframes; i++) {
+        let atoms = [];
+        for (let j = 0; j < natoms; j++) {
+            let line = lines[i * (natoms + 2) + j + 2].split(/\s+/);
+            let atom = {};
+            let k = 0;
             while (line[k] == "") k++;
             atom.symbol = line[k++];
             atom.position = [
